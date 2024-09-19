d27a6b83f6b840963c06d5ce55465435f70c9d13
Lars Grammel <lars.grammel@gmail.com>
2024-09-19 10:29:59 +0200

    Organize middleware modules (#1)
    * separate file for middleware
    
    * inline to avoid type issue
    
    * improvements
    
    * more improvements
    
    * tweaks
    
    * Update pnpm-lock file
    
    ---------
    
    Co-authored-by: Jeremy <git@jrmyphlmn.com>

---
+++ ai/rag-middleware.ts
@@ -0,0 +1,106 @@
+import { auth } from "@/app/auth";
+import { getChunksByFilePaths } from "@/app/db";
+import { openai } from "@ai-sdk/openai";
+import {
+  cosineSimilarity,
+  embed,
+  Experimental_LanguageModelV1Middleware,
+  generateObject,
+  generateText,
+} from "ai";
+import { z } from "zod";
+
+// schema for validating the custom provider metadata
+const selectionSchema = z.object({
+  files: z.object({
+    selection: z.array(z.string()),
+  }),
+});
+
+export const ragMiddleware: Experimental_LanguageModelV1Middleware = {
+  transformParams: async ({ params }) => {
+    const session = await auth();
+
+    if (!session) return params; // no user session
+
+    const { prompt: messages, providerMetadata } = params;
+
+    // validate the provider metadata with Zod:
+    const { success, data } = selectionSchema.safeParse(providerMetadata);
+
+    if (!success) return params; // no files selected
+
+    const selection = data.files.selection;
+
+    const recentMessage = messages.pop();
+
+    if (!recentMessage || recentMessage.role !== "user") return params;
+
+    const lastUserMessageContent = recentMessage.content
+      .filter((content) => content.type === "text")
+      .map((content) => content.text)
+      .join("\n");
+
+    // Classify the user prompt as whether it requires more context or not
+    const { object: classification } = await generateObject({
+      // fast model for classification:
+      model: openai("gpt-4o-mini", { structuredOutputs: true }),
+      output: "enum",
+      enum: ["question", "statement", "other"],
+      system: "classify the user message as a question, statement, or other",
+      prompt: lastUserMessageContent,
+    });
+
+    if (classification !== "question") return params; // only use RAG for questions
+
+    // Use hypothetical document embeddings:
+    const { text: hypotheticalAnswer } = await generateText({
+      // fast model for generating hypothetical answer:
+      model: openai("gpt-4o-mini", { structuredOutputs: true }),
+      system: "Answer the users question:",
+      prompt: lastUserMessageContent,
+    });
+
+    // Embed the hypothetical answer
+    const { embedding: hypotheticalAnswerEmbedding } = await embed({
+      model: openai.embedding("text-embedding-3-small"),
+      value: hypotheticalAnswer,
+    });
+
+    // find relevant chunks based on the selection
+    const chunksBySelection = await getChunksByFilePaths({
+      filePaths: selection.map((path) => `${session.user?.email}/${path}`),
+    });
+
+    const chunksWithSimilarity = chunksBySelection.map((chunk) => ({
+      ...chunk,
+      similarity: cosineSimilarity(
+        hypotheticalAnswerEmbedding,
+        chunk.embedding
+      ),
+    }));
+
+    // rank the chunks by similarity and take the top K
+    chunksWithSimilarity.sort((a, b) => b.similarity - a.similarity);
+    const k = 10;
+    const topKChunks = chunksWithSimilarity.slice(0, k);
+
+    // add the chunks to the last user message
+    messages.push({
+      role: "user",
+      content: [
+        ...recentMessage.content,
+        {
+          type: "text",
+          text: "Here is some relevant information that you can use to answer the question:",
+        },
+        ...topKChunks.map((chunk) => ({
+          type: "text" as const,
+          text: chunk.content,
+        })),
+      ],
+    });
+
+    return { ...params, prompt: messages };
+  },
+};
